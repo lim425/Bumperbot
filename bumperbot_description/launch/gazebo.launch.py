3dc4842d3b2e3fd72385c9d22b3d356085cb5748
desmond <limjunyi425@gmail.com>
2025-04-11 17:25:24 +0800

    Add robot urdf and gazebo

---
+++ bumperbot_description/launch/gazebo.launch.py
@@ -0,0 +1,51 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable, IncludeLaunchDescription
+from launch.substitutions import Command, LaunchConfiguration
+import os
+from ament_index_python.packages import get_package_share_directory, get_package_prefix
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def generate_launch_description():
+
+    model_arg = DeclareLaunchArgument(
+        name="model",
+        default_value=os.path.join(get_package_share_directory("bumperbot_description"), "urdf", "bumperbot.urdf.xacro"),
+        description="Absolute path to the robot URDF file"
+    )
+
+    robot_description = ParameterValue(Command(["xacro ", LaunchConfiguration("model")]))
+
+    robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[{"robot_description": robot_description}]
+    )
+
+    env_var = SetEnvironmentVariable("GAZEBO_MODEL_PATH", os.path.join(get_package_prefix("bumperbot_description"), "share"))
+
+    start_gazebo_server = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(get_package_share_directory("gazebo_ros"), "launch", "gzserver.launch.py"))
+    )
+
+    start_gazebo_client = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(get_package_share_directory("gazebo_ros"), "launch", "gzclient.launch.py"))
+    )
+
+    spawn_robot = Node(
+        package="gazebo_ros",
+        executable="spawn_entity.py",
+        # arguments=["-entity", "bumperbot", "-topic", "robot_description", "-x", "0", "-y", "0", "-z", "0"]
+        arguments=["-entity", "bumperbot", "-topic", "robot_description"],
+        output="screen"
+    )
+
+    return LaunchDescription([
+        model_arg,
+        env_var,
+        robot_state_publisher,
+        start_gazebo_server,
+        start_gazebo_client,
+        spawn_robot
+    ])
